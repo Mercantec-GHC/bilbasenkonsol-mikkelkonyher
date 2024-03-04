bf99b5a0dbd1b16050c3e2e316dd6d436f35fa1a
Mikkel konyher <mikkelkonyher@gmail.com>
2024-03-04 11:06:05 +0100

    OOP Dag 1 V1

--- obj/Debug/net8.0/BilbasenKonsol.AssemblyInfo.cs
+++ obj/Debug/net8.0/BilbasenKonsol.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BilbasenKonsol")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f25497bb3afdd04c1b782ddd7ad70b6d05553d56")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+342bf411eeca8040780bab072694eb44a0cb1923")]
 [assembly: System.Reflection.AssemblyProductAttribute("BilbasenKonsol")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BilbasenKonsol")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
